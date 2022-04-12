a59d114b61d66310225c726706b9f40798cee1c8
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2022-04-12 19:24:03 +0200

    - added store and user reducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import Navigation from 'routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
@@ -8,21 +9,36 @@ import Shop from './routes/shop/shop.component';
 import Checkout from 'routes/checkout/checkout.component';
 import CategoryPage from 'components/category-page/category-page.component';
 
+import { createUserDocumentFromAuth, onAuthStateChangedListener } from 'utils/firebase/firebase.utils';
+
+import { setCurrentUser } from 'store/user/user.action';
+
 import './App.css';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) createUserDocumentFromAuth(user);
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+    // dispatch never changes
+  }, [dispatch]);
+
   return (
     <Routes>
-      <Route path='/' element={<Navigation />} >
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop/*' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
         <Route path='checkout' element={<Checkout />} />
         <Route path='category-page' element={<CategoryPage />} />
       </Route>
-
     </Routes>
-  )
-}
+  );
+};
 
 export default App;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,50 +1,45 @@
 import React, { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from 'store/user/user.selector.js';
 
 import CartIcon from 'components/cart-icon/cart-icon.component';
 import CartDropdown from 'components/cart-dropdown/cart-dropdown.component';
 
-import { UserContext } from 'contexts/user.context';
 import { CartContext } from 'contexts/cart.context';
 import { signOutUser } from 'utils/firebase/firebase.utils';
 
-import {
-    NavigationContainer,
-    LogoContainer,
-    NavLinksContainer,
-    NavLink
-} from './navigation.styles.jsx';
+import { NavigationContainer, LogoContainer, NavLinksContainer, NavLink } from './navigation.styles.jsx';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
-    const { isCartOpen } = useContext(CartContext);
+  const currentUser = useSelector(selectCurrentUser);
+  const { isCartOpen } = useContext(CartContext);
 
-    return (
-        <Fragment>
-            <NavigationContainer>
-                <LogoContainer to='/'>
-                    <CrwnLogo className='logo' />
-                </LogoContainer>
-                <NavLinksContainer>
-                    <NavLink to='/shop'>
-                        SHOP
-                    </NavLink>
-                    {currentUser ? (
-                        <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>
-                    ) : (
-                        <NavLink to='/auth'>
-                            SIGN IN
-                        </NavLink>
-                    )}
-                    <CartIcon />
-                </NavLinksContainer>
-                {isCartOpen && <CartDropdown />}
-            </NavigationContainer>
-            <Outlet />
-        </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      <NavigationContainer>
+        <LogoContainer to='/'>
+          <CrwnLogo className='logo' />
+        </LogoContainer>
+        <NavLinksContainer>
+          <NavLink to='/shop'>SHOP</NavLink>
+          {currentUser ? (
+            <NavLink as='span' onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
+          ) : (
+            <NavLink to='/auth'>SIGN IN</NavLink>
+          )}
+          <CartIcon />
+        </NavLinksContainer>
+        {isCartOpen && <CartDropdown />}
+      </NavigationContainer>
+      <Outlet />
+    </Fragment>
+  );
+};
 
 export default Navigation;
