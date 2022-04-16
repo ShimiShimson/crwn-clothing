8dbd2f687112569e12e99fca6778801673688c0d
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2022-04-16 09:02:19 +0200

    - fix error in definition of selectCategoriesMap

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -4,11 +4,10 @@ const selectCategoriesReducer = (state) => state.categories;
 
 const selectCategories = createSelector([selectCategoriesReducer], (categoriesSlice) => categoriesSlice.categories);
 
-export const selectCategoriesMap = () =>
-  createSelector([selectCategories], (categories) =>
-    categories.reduce((acc, category) => {
-      const { title, items } = category;
-      acc[title.toLowerCase()] = items;
-      return acc;
-    }, {})
-  );
+export const selectCategoriesMap = createSelector([selectCategories], (categories) =>
+  categories.reduce((acc, category) => {
+    const { title, items } = category;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
+);
